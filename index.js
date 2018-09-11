a27dff6f66bd1559e343bbc68799af6f2e98398c
Guillermo Tala <guille86598@gmail.com>
2018-09-11 03:19:04 +0000

    msj

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var Web3 = require("web3"); 
+/*ar Web3 = require("web3"); 
 
 const testnet = 'https://ropsten.infura.io/';
 const walletAddress = '0x72b7d5dff8509f321a40e28e37f18b3198a26cbe';
@@ -7,10 +7,11 @@ const web3 = new Web3(new Web3.providers.HttpProvider(testnet));
 var balance = web3.eth.getBalance(walletAddress); //Will give value in.
 balance = web3.toDecimal(balance);
 console.log(balance);
+*/
 
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write(balance.toString());
+  res.write("Server is running...");
   res.end();
 }).listen(8080);
